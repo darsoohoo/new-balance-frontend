d47106459c3d3780b47959b0fe73b62a740aa05a
Darren SooHoo <darsoohoo@gmail.com>
2023-10-09 14:43:36 -0700

    build eploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,19 @@ export default defineConfig( ({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
-  console.log("env issssssssssssssssss ", env)
+  console.log("command -", command)
+  console.log("mode -", mode)
+console.log("env.VITE_API_ENDPOINT", env.VITE_API_ENDPOINT)
+
   return {
           server: {
             proxy: {
               "/api": env.VITE_API_ENDPOINT,
             },
           },
-          plugins: [react()]
+          plugins: [react()],
+          define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+          },
     }
 });
